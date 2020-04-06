7f73b5c7bbb2c80feee8e32d4f3ec44018680909
QA engineer <kotarakh@gmail.com>
2020-04-06 22:17:43 +0300

    Initial commit

---
+++ src/main/java/CollectionsHWRun.java
@@ -0,0 +1,10 @@
+import org.apache.log4j.Logger;
+
+public class CollectionsHWRun {
+    public static void main(String[] args) {
+
+        final Logger log = Logger.getLogger("homeworKKK");
+
+        log.info("this is info");
+    }
+}
---
+++ src/main/java/Tasks.java
@@ -0,0 +1,5 @@
+public class Tasks {
+
+    private String lorem = "It is a devastated history, sir.Cur sensorem unda?Avast, love me sailor, ye weird lubber!";
+
+}
